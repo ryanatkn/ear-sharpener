3fe74c08e3b1aecc29cb1c51cb0e8c1ef1b9fd01
Ryan Atkinson <ryanatkn@gmail.com>
2016-06-29 15:57:26 -0600

    pin typescript version to the june 19 nightly to avoid compilation errors with the latest

--- src/middleware/localStorageStoreEnhancer/test.ts
+++ src/middleware/localStorageStoreEnhancer/test.ts
@@ -1,9 +1,9 @@
-import localStorageMiddleware from './index';
+import localStorageStoreEnhancer from './index';
 import {assert} from 'chai';
 
-describe('localStorageMiddleware', () => {
+describe('localStorageStoreEnhancer', () => {
   // Can't test in node without a fake dom, so do a basic smoke test.
   it('should not blow up', () => {
-    assert(localStorageMiddleware()());
+    assert(localStorageStoreEnhancer()());
   });
 });
