d0216da6ca6383cdde36c3f4400a3f81afd8095a
Ryan Atkinson <mail@ryanatkn.com>
2021-05-12 14:34:37 -0600

    add a disclaimer

--- src/containers/IndexPage/index.tsx
+++ src/containers/IndexPage/index.tsx
@@ -16,6 +16,11 @@ export default class IndexPage extends React.Component<{}, {}> {
           The <Link to="/piano-game">Piano Game</Link> is the easiest one to start with.
         </p>
         <p>
+          <strong>Please note:</strong> the piano sounds are
+          <a href="https://github.com/ryanatkn/ear-sharpener/issues/2">a little off pitch</a>.
+          You probably shouldn't play it too long.
+        </p>
+        <p>
           Source code is available <a href="https://github.com/ryanatkn/ear-sharpener">here on Github.</a> This
           website is made with TypeScript, React, Redux, Immutable.js, React Router, PostCSS, Mocha, Chai,
           Sinon, Enzyme, Webpack, and the Redux DevTools Extension.
